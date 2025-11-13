fe70c5d41cb410e8b2727a0031edee3e0d602b5a
kuko010 <kukouwu@proton.me>
2025-11-13 17:21:30 +0100

    Initial commit

---
+++ src/main/java/net/kuko/fisch/Fisch.java
@@ -0,0 +1,16 @@
+package net.kuko.fisch;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Fisch implements ModInitializer {
+	public static final String MOD_ID = "fisch";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}

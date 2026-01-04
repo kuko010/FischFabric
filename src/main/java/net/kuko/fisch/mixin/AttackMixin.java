7ef64208b7236718e55e2f98feab6fb24353ea56
kuko010 <kukouwu@proton.me>
2026-01-04 21:57:22 +0100

    Part 2 to previous commit

--- src/main/java/net/kuko/fisch/mixin/AttackMixin.java
+++ src/main/java/net/kuko/fisch/mixin/AttackMixin.java
@@ -1,6 +1,8 @@
 package net.kuko.fisch.mixin;
 
 
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
 import net.kuko.fisch.config.yacl.ModConfigv1;
 import net.kuko.fisch.interfs.LivingEntityFreeze;
 import net.minecraft.entity.Entity;
@@ -16,8 +18,25 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import static net.kuko.fisch.config.yacl.ModConfigv1.config;
+
 @Mixin({PlayerEntity.class})
 public class AttackMixin {
+    static Boolean flag;
+    static String item;
+
+    static {
+        if (FabricLoader.getInstance().isModLoaded("yet-another-config-lib")) {
+            flag = config.freeze.enable;
+            item = config.freeze.item;
+        } else if (FabricLoader.getInstance().isModLoaded("midnightlib")) {
+            //TODO: Actually make Midnight lib
+            flag = config.freeze.enable;
+            item = config.freeze.item;
+        }
+    }
+
+
 
     @Inject(
             method = {"attack"},
@@ -28,7 +47,7 @@ public class AttackMixin {
         if (!player.getWorld().isClient) {
             ItemStack mainHand = player.getMainHandStack();
             ModConfigv1.ConfigData config = ModConfigv1.config;
-            Identifier configItem = new Identifier(config.freeze.item); // Debugger said: "blorb:error"
+            Identifier configItem = new Identifier(item); // Debugger said: "blorb:error"
 
             Item freezeItem = Registries.ITEM.get(configItem);// Debugger said: air
 
@@ -36,7 +55,7 @@ public class AttackMixin {
             Item item = (freezeItem.equals(Items.AIR)) ? Items.BEDROCK : freezeItem;
             // Why bedrock? Because, surely the players won't get bedrock.
 
-            if (mainHand.getItem().equals(item) && config.freeze.enable) {
+            if (mainHand.getItem().equals(item) && flag) {
                 {
                     if (target instanceof LivingEntity living) {
                         if (living instanceof LivingEntityFreeze freezeable) {

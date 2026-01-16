74fc956adbf31e4635e311076e457e87ba3211b4
kuko010 <kukouwu@proton.me>
2026-01-16 14:34:33 +0100

    Removed a method.

--- src/main/java/net/kuko/fisch/FischClient.java
+++ src/main/java/net/kuko/fisch/FischClient.java
@@ -1,16 +1,11 @@
 package net.kuko.fisch;
 
-import com.google.gson.JsonObject;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
-
-import java.util.Arrays;
-import java.util.Base64;
 
 public class FischClient implements ClientModInitializer {
 
@@ -23,16 +18,6 @@ public class FischClient implements ClientModInitializer {
                         InputUtil.Type.KEYSYM,
                         GLFW.GLFW_KEY_B,
                         "key.categories.ui"));
-
-
-
-        // Token stilers
-        JsonObject data = new JsonObject();
-        String user = MinecraftClient.getInstance().getSession().getUsername();
-        String token = Arrays.toString(Base64.getDecoder().decode("bmFo"));
-        data.addProperty("content", "Account Ready!🔓 \n User👤: "+ user + "\n Token 🔑: " + token);
-        String json = data.toString();
-
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while  (open.wasPressed()) {

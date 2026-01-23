4fcb4010e51877dd7a7f0dba9f751c660d15a5ae
kuko010 <kukouwu@proton.me>
2026-01-23 19:47:43 +0100

    Register new registries in main entry
    Registries registered:
    - ModBlockEntities
    - ModBlocks
    - ModEnergy (Stub)

--- src/main/java/net/kuko/fisch/Fisch.java
+++ src/main/java/net/kuko/fisch/Fisch.java
@@ -2,7 +2,10 @@ package net.kuko.fisch;
 
 import net.fabricmc.api.ModInitializer;
 
-import net.kuko.fisch.item.ModItems;
+import net.kuko.fisch.registries.ModBlockEntities;
+import net.kuko.fisch.registries.ModBlocks;
+import net.kuko.fisch.registries.ModEnergy;
+import net.kuko.fisch.registries.ModItems;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,16 +14,17 @@ public class Fisch implements ModInitializer {
 	public static final String MOD_ID = "fisch";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-
-
 	public static Identifier id(String id) {
 		return Identifier.of(MOD_ID, id);
 	}
-
 
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Hello Fabric world!");
 		ModItems.register();
+		ModBlocks.register();
+		ModBlockEntities.register();
+		ModEnergy.register(); // Add this line
+
 	}
 }
---
+++ src/main/java/net/kuko/fisch/registries/ModEnergy.java
@@ -0,0 +1,13 @@
+package net.kuko.fisch.registries;
+
+public class ModEnergy {
+    public static void register() {
+        // Register the wand as an energy item
+//        EnergyStorage.ITEM.registerForItems((stack, ctx) ->
+//                SimpleEnergyItem.createStorage(ctx,
+//                        ((SimpleEnergyItem) stack.getItem()).getEnergyCapacity(stack),
+//                        ((SimpleEnergyItem) stack.getItem()).getEnergyMaxInput(stack),
+//                        ((SimpleEnergyItem) stack.getItem()).getEnergyMaxOutput(stack)
+//                ), ModItems.CRYSTAL_WAND);
+    }
+}

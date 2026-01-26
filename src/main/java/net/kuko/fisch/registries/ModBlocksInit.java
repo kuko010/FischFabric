aea7edb8494a3bcafc4bfa5f3b25b84e01db76c4
kuko010 <kukouwu@proton.me>
2026-01-26 21:53:26 +0100

    (Forgot to commit this change)

---
+++ src/main/java/net/kuko/fisch/registries/ModBlocksInit.java
@@ -0,0 +1,19 @@
+package net.kuko.fisch.registries;
+
+
+import io.wispforest.owo.registration.reflect.BlockRegistryContainer;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.kuko.fisch.block.ExamplePedestal;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+
+public class ModBlocksInit implements BlockRegistryContainer {
+    public static final Block EXAMPLE_PEDESTAL = new ExamplePedestal(FabricBlockSettings.create());
+
+
+    @Override
+    public BlockItem createBlockItem(Block block, String identifier) {
+        return new BlockItem(block, new Item.Settings());
+    }
+}

3a71c73d6f637c552003bbf0b5ddaef554473a53
kuko010 <kukouwu@proton.me>
2026-01-26 21:56:06 +0100

    License Changed in fabric.mod.json clean up. Remove modmenu from fabric.mod.json Add owo-lib into fabric.mod.json depends Remove invalid Mixin

--- src/main/java/net/kuko/fisch/FischClient.java
+++ src/main/java/net/kuko/fisch/FischClient.java
@@ -3,6 +3,8 @@ package net.kuko.fisch;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.rendering.v1.BlockEntityRendererRegistry;
+import net.kuko.fisch.registries.ModBlockEntitiesInit;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
@@ -27,10 +29,7 @@ public class FischClient implements ClientModInitializer {
             }
         });
 
-        // Register screen
-//        ScreenRegistry.register(
-//                ModScreenHandlers.CRYSTAL_CHARGER_SCREEN_HANDLER,
-//                CrystalChargerScreen::new
-//        );
+//        BlockEntityRendererRegistry.register(ModBlockEntitiesInit.EXAMPLE_PEDESTAL_BE, MyBlockEntityRenderer::new);
+
     }
 }

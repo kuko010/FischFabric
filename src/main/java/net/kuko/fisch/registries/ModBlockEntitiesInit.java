5d49c666568068e2afa3202657e2fc20c374d0ac
kuko010 <kukouwu@proton.me>
2026-01-30 13:54:42 +0100

    Removed the pedestal entity block

--- src/main/java/net/kuko/fisch/registries/ModBlockEntitiesInit.java
+++ src/main/java/net/kuko/fisch/registries/ModBlockEntitiesInit.java
@@ -1,15 +1,13 @@
 package net.kuko.fisch.registries;
 
 import io.wispforest.owo.registration.reflect.AutoRegistryContainer;
-import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
-import net.kuko.fisch.block.entity.ExamplePedestalBlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 
 public class ModBlockEntitiesInit implements AutoRegistryContainer<BlockEntityType<?>> {
-    public static final BlockEntityType<ExamplePedestalBlockEntity> EXAMPLE_PEDESTAL_BE = FabricBlockEntityTypeBuilder.create(ExamplePedestalBlockEntity::new,
-            ModBlocksInit.EXAMPLE_PEDESTAL).build();
+//    public static final BlockEntityType<ExamplePedestalBlockEntity> EXAMPLE_PEDESTAL_BE = FabricBlockEntityTypeBuilder.create(ExamplePedestalBlockEntity::new,
+//            ModBlocksInit.EXAMPLE_PEDESTAL).build();
 
     @Override
     public Registry<BlockEntityType<?>> getRegistry() {
--- src/main/java/net/kuko/fisch/registries/ModBlocksInit.java
+++ src/main/java/net/kuko/fisch/registries/ModBlocksInit.java
@@ -2,14 +2,12 @@ package net.kuko.fisch.registries;
 
 
 import io.wispforest.owo.registration.reflect.BlockRegistryContainer;
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.kuko.fisch.block.ExamplePedestal;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 
 public class ModBlocksInit implements BlockRegistryContainer {
-    public static final Block EXAMPLE_PEDESTAL = new ExamplePedestal(FabricBlockSettings.create());
+//    public static final Block EXAMPLE_PEDESTAL = new ExamplePedestal(FabricBlockSettings.create());
 
 
     @Override

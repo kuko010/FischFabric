b73becacae26913232733f188a7cbcdcd3619e7b
kuko010 <kukouwu@proton.me>
2026-02-03 18:52:18 +0100

    feat called Stain API
    enables shader "testShader" programmatically
    on "open" keybind, enable and disable "testShader" shader.

--- src/main/java/net/kuko/fisch/FischClient.java
+++ src/main/java/net/kuko/fisch/FischClient.java
@@ -1,13 +1,24 @@
 package net.kuko.fisch;
 
+import ladysnake.satin.api.event.ShaderEffectRenderCallback;
+import ladysnake.satin.api.managed.ManagedShaderEffect;
+import ladysnake.satin.api.managed.ShaderEffectManager;
+import ladysnake.satin.api.managed.uniform.Uniform4f;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.util.Identifier;
 import org.lwjgl.glfw.GLFW;
 
 public class FischClient implements ClientModInitializer {
+    /* To be deleted!!!! */
+    private boolean renderingBlit = false;
+    // literally the same as minecraft's blit, we are just checking that custom paths work
+    private final ManagedShaderEffect testShader = ShaderEffectManager.getInstance().manage(
+            new Identifier(Fisch.MOD_ID, "shaders/post/blit.json"));
+    private final Uniform4f color = testShader.findUniform4f("ColorModulate");
 
 
     @Override
@@ -19,15 +30,23 @@ public class FischClient implements ClientModInitializer {
                         GLFW.GLFW_KEY_B,
                         "key.categories.ui"));
 
+        /*
+
+
+
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while  (open.wasPressed()) {
                // client.player.sendMessage(Text.literal("Key 1 was pressed!"), false);
                 //client.player
 //                MinecraftClient.getInstance().setScreen(new OdeToBeansScreen());
+                renderingBlit = !renderingBlit;
+//                color.set((float) Math.random(), (float) Math.random(), (float) Math.random(), 1.0f);
             }
         });
-
-//        BlockEntityRendererRegistry.register(ModBlockEntitiesInit.EXAMPLE_PEDESTAL_BE, MyBlockEntityRenderer::new);
-
+        ShaderEffectRenderCallback.EVENT.register(tickDelta -> {
+            if (renderingBlit) {
+                testShader.render(tickDelta);
+            }
+        });*/
     }
 }

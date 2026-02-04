14002acb4e4f0a14bd0848a3dbb6097aefb064be
kuko010 <kukouwu@proton.me>
2026-02-04 15:20:26 +0100

    Removed shaders

--- src/main/java/net/kuko/fisch/FischClient.java
+++ src/main/java/net/kuko/fisch/FischClient.java
@@ -14,16 +14,6 @@ import net.minecraft.util.Identifier;
 import org.lwjgl.glfw.GLFW;
 
 public class FischClient implements ClientModInitializer {
-    private boolean enableShader = false;
-
-    private final ManagedShaderEffect outlineShader = ShaderEffectManager.getInstance()
-            .manage(new Identifier(Fisch.MOD_ID, "shaders/post/outline.json"));
-
-    // Access to shader uniforms for customization
-    private final Uniform1f edgeThickness = outlineShader.findUniform1f("EdgeThickness");
-    private final Uniform1f edgeStrength = outlineShader.findUniform1f("EdgeStrength");
-    private final Uniform3f outlineColor = outlineShader.findUniform3f("OutlineColor");
-
     @Override
     public void onInitializeClient() {
         KeyBinding open = KeyBindingHelper.registerKeyBinding(
@@ -32,22 +22,5 @@ public class FischClient implements ClientModInitializer {
                         InputUtil.Type.KEYSYM,
                         GLFW.GLFW_KEY_B,
                         "key.categories.ui"));
-
-        // Set initial values
-        edgeThickness.set(10.0f);
-        edgeStrength.set(500.0f);
-        outlineColor.set(0.0f, 0.0f, 0.0f); // Black outlines
-
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (open.wasPressed()) {
-                enableShader = !enableShader;
-            }
-        });
-
-        ShaderEffectRenderCallback.EVENT.register(tickDelta -> {
-            if (enableShader) {
-                outlineShader.render(tickDelta);
-            }
-        });
     }
 }

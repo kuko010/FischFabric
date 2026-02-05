6e766c6873cedccfd149cd693c76ca067c0d5a98
kuko010 <kukouwu@proton.me>
2026-02-05 17:03:49 +0100

    add a way to open the screen

--- src/main/java/net/kuko/fisch/FischClient.java
+++ src/main/java/net/kuko/fisch/FischClient.java
@@ -1,26 +1,35 @@
 package net.kuko.fisch;
 
-import ladysnake.satin.api.event.ShaderEffectRenderCallback;
-import ladysnake.satin.api.managed.ManagedShaderEffect;
-import ladysnake.satin.api.managed.ShaderEffectManager;
-import ladysnake.satin.api.managed.uniform.Uniform1f;
-import ladysnake.satin.api.managed.uniform.Uniform3f;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.kuko.fisch.client.screen.TheScreen;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.util.Identifier;
 import org.lwjgl.glfw.GLFW;
 
 public class FischClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        KeyBinding open = KeyBindingHelper.registerKeyBinding(
+        KeyBinding openScreen = KeyBindingHelper.registerKeyBinding(
                 new KeyBinding(
-                        "key." + Fisch.MOD_ID + ".open_paper",
+                        "key.fisch.open_paper",
                         InputUtil.Type.KEYSYM,
                         GLFW.GLFW_KEY_B,
-                        "key.categories.ui"));
+                        "key.categories.ui"
+                ));
+
+
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (openScreen.isPressed()) {
+                if (client.player != null) {
+//                    client.player.displayClientMessage(Component.literal("Key Pressed!"), false);
+                    MinecraftClient mc = MinecraftClient.getInstance();
+                    mc.setScreen(new TheScreen());
+                }
+            }
+        });
     }
 }

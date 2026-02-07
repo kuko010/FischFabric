3ba5941fee79172fecb4407d7e18a1d0336548e2
kuko010 <kukouwu@proton.me>
2026-02-07 10:24:37 +0100

    feat: Added printing message for NewItem.kt

--- src/main/kotlin/net/kuko/fisch/item/NewItem.kt
+++ src/main/kotlin/net/kuko/fisch/item/NewItem.kt
@@ -1,7 +1,31 @@
 package net.kuko.fisch.item
 
+import net.kuko.fisch.Fisch
+import net.kyori.adventure.platform.fabric.FabricAudiences
+import net.kyori.adventure.platform.fabric.FabricClientAudiences
+import net.kyori.adventure.text.minimessage.MiniMessage
+import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.Item
+import net.minecraft.item.ItemStack
+import net.minecraft.text.Text
+import net.minecraft.util.Hand
+import net.minecraft.util.TypedActionResult
+import net.minecraft.world.World
+
 
 class NewItem(settings: Settings) : Item(settings) {
     // change
+
+
+    override fun use(world: World, user: PlayerEntity, hand: Hand?): TypedActionResult<ItemStack?>? {
+        if (world.isClient()) {
+            val mm: MiniMessage = MiniMessage.miniMessage()
+            val parsed = mm.deserialize("Hello <rainbow>world</rainbow>, isn't <underlined>MiniMessage</underlined> fun?")
+            val text = FabricClientAudiences.of().toNative(parsed)
+            user.sendMessage(text);
+        }
+        Fisch.LOGGER.info("Sent text!")
+
+        return TypedActionResult.success(user.mainHandStack)
+    }
 }

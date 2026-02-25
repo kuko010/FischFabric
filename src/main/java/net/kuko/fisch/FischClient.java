c703a64ca2934715adc9839ac239e73832fce0bb
kuko010 <kukouwu@proton.me>
2026-02-25 22:17:18 +0100

    fix: minor bug

--- src/main/java/net/kuko/fisch/FischClient.java
+++ src/main/java/net/kuko/fisch/FischClient.java
@@ -3,7 +3,6 @@ package net.kuko.fisch;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.kuko.fisch.client.screen.TheScreen;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
@@ -25,7 +24,7 @@ public class FischClient implements ClientModInitializer {
                 if (client.player != null) {
 //                    client.player.displayClientMessage(Component.literal("Key Pressed!"), false);
                     MinecraftClient mc = MinecraftClient.getInstance();
-                    mc.setScreen(new TheScreen());
+                 //   mc.setScreen(new TheScreen());
                 }
             }
         });
